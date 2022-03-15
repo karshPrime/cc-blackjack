4a24d13b312848b34143958cb2cb062947515834
ut-kr <utkarsh@cyberfear.com>
2022-03-15 16:23:14 +1100

    ✨ heading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void head();
+
 int main() {
-  // 
+  //
   return 0;
+}
+
+// print heading information on user console
+void head() {
+  std::cout << " _     _            _     _            _" << std::endl;
+  std::cout << "| |__ | | __ _  ___| | __(_) __ _  ___| | __" << std::endl;
+  std::cout << "| '_ \| |/ _` |/ __| |/ /| |/ _` |/ __| |/ /" << std::endl;
+  std::cout << "| |_) | | (_| | (__|   < | | (_| | (__|   <" << std::endl;
+  std::cout << "|_.__/|_|\__,_|\___|_|\__/ |\__,_|\___|_|\_\" << std::endl;
+  std::cout << "                       |__/" << std::endl;
+
 }
